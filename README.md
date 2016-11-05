13e36855156e1d6f5514426da674b4ca3454c5ad
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-11-05 10:59:01 +0900

    Initial commit
    Fleshed our README a bit, tweaked the cassandra profile module.
    Still the same as the ML repo version.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Melanippe
 Python CLI tool for profiling databases
+
+Started as a one-off script in my ML repo.
+Going to try making it into something more useful.
+
+Goal:
+
+Quickly get a lot of useful information about your database. Things like:
+
+Size on disk
+Table metadata
+Schema
+Configuration
+
+Useful info is exported to csv.
+
+You can then do a cron and regularly take diffs to see how things change.
+You can also export to Google Docs.
+
+TODO:
+  - Add configuration / setup
+  - Add intelligence to support different environments and OS's.
+  - Add functionality to export to Google docs
+  - Add postgresql support
+  - Add mongodb support
+  - Add sqlite support
+  - Add tests
---
+++ cassandra_profile.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from cassandra.cluster import Cluster
+import sys
+import csv
+import os
+
+if len(sys.argv) < 2:
+    print "ERROR: Keyspace name not provided."
+    exit(1)
+else:
+    keyspace_name = sys.argv[1]
+
+schema_query = '''SELECT * FROM system_schema.tables \
+                WHERE keyspace_name=%s;'''
+
+columns_query = '''SELECT keyspace_name, table_name, column_name \
+                FROM system_schema.columns WHERE keyspace_name=%s'''
+
+du_command = "echo 'size_on_disk',`du -hc /var/lib/cassandra/data/%s/%s* \
+            | grep total | awk '{print $1}'`"
+
+def setup():
+    cluster = Cluster()
+    session = cluster.connect()
+    return session
+
+def generate_csv(dlist):
+    for d in dlist:
+        name = d['table_name'] + ".csv"
+        with open(name, 'wb') as f:
+            w = csv.writer(f)
+            w.writerows(d.items())
+        f.close()
+
+def add_table_size(dicts):
+    # Using os instead of subprocess because I miss 2.6...
+    for thing in dicts:
+        out = os.popen(du_command % (thing['keyspace_name'], thing['table_name']))
+        a = out.read().rstrip()
+        vals = a.split(",")
+        thing[vals[0]] = vals[1]
+    return dicts
+
+def get_table_schemas(session, keyspace_name):
+    result = session.execute(schema_query, (keyspace_name,))
+    dicts = []
+    for a in result:
+        a = a.__dict__
+        dicts.append(a)
+    return dicts
+
+def add_columns():
+    pass
+
+def profile_tables(session, keyspace_name):
+    a = get_table_schemas(session, keyspace_name)
+    # b = add_columns(a)
+    c = add_table_size(a)
+    return c
+
+if __name__ == "__main__":
+    session = setup()
+    dicts = profile_tables(session, keyspace_name)
+    generate_csv(dicts)
